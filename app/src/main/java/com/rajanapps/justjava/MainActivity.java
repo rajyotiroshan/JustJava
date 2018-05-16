8359e9465d404de4791c5082569169dafc09bf0e
rajan jha <rajahit16@gmail.com>
2018-05-16 23:32:06 +0530

    condition for <1 and >100 coffee

--- app/src/main/java/com/rajanapps/justjava/MainActivity.java
+++ app/src/main/java/com/rajanapps/justjava/MainActivity.java
@@ -6,12 +6,12 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
     private int quantity = 0;
     private int pricePerCoffee = 5;
-    private float basePrice =0;
     private String msg ="";
 
     @Override
@@ -27,15 +27,20 @@ public class MainActivity extends AppCompatActivity {
      */
     private void displayQuantity(int number) {
         TextView quantityTV = findViewById(R.id.quantity_text_view);
-        quantityTV.setText(number + "");
+        quantityTV.setText(String.valueOf(number));
     }
 
     /**
      * decrease quantity by on clicking - button
      *
-     * @param view
+     * @param view decrease button
      */
     public void decrease(View view) {
+        if(quantity-1 < 1){
+            Toast toast = Toast.makeText(this,"Really, At least one coffee!",Toast.LENGTH_LONG);
+            toast.show();
+            return;
+        }
         quantity--;
         displayQuantity(quantity);
 
@@ -44,9 +49,13 @@ public class MainActivity extends AppCompatActivity {
     /**
      * increase quantity by 1 on clicking + button
      *
-     * @param view
+     * @param view decrease button.
      */
     public void increase(View view) {
+        if(quantity + 1 >100){
+            Toast.makeText(this , "Nah,It's too much!", Toast.LENGTH_SHORT).show();
+            return;
+        }
         quantity++;
         displayQuantity(quantity);
     }
@@ -55,11 +64,15 @@ public class MainActivity extends AppCompatActivity {
      * called when Order button is clicked
      */
     public void submitOrder(View view) {
-        basePrice = quantity * pricePerCoffee;
+        int totalPrice = 0;
+        int basePrice = 0;
+        //price of coffee + any toppings.
+        basePrice = pricePerCoffee;
+        totalPrice = basePrice * quantity;
         EditText nameET = (EditText)findViewById(R.id.name);
         msg = String.valueOf(nameET.getText());
         msg += "\nquantity: "+ quantity;
-        msg += "\nprice: $" +basePrice;
+        msg += "\nprice: $" +totalPrice;
         msg += "\nThank You";
         TextView priceTV = (TextView) findViewById(R.id.price_text_view);
         priceTV.setText(msg);
